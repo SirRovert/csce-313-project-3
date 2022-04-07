41e7c32ace8c791babd577d7c46738e9c490c8a6
jadepanths <jadepanths@gmail.com>
2022-04-07 05:26:23 -0500

    added fuel estimation using avg miles/US gallon and user's inputs

--- express/js/fuel-price.js
+++ express/js/fuel-price.js
@@ -1,3 +1,5 @@
+/*
+// since the api doesn't work, we let the users input the current gas price
 function setup() {
     createCanvas(200,200);
     // HERE Api
@@ -7,10 +9,30 @@ function setup() {
     let api_key = "&apiKey=jd8v2XkEgHFAXEum_Cg0o9o3_a6DfBOtUiakZE1AN2c"
     loadJSON("api");
 }
+*/
 
-function GetFuelPrice(distance, type) {
-    let price = distance * 20;
-    return price;
+const fuelCost = document.getElementById("fuelCost");
+
+function getGasPrice() {
+    const val = document.querySelector('input').value;
+    console.log(val);
+    return val;
+}
+
+function getDistance() {
+    const val = document.querySelector('input').value;
+    console.log(val);
+    return val;
+}
+
+function eastimateCost() {
+    let dis = getDistance();
+    let fp = getGasPrice();
+    
+    const milePerGallon = 25.4;
+    let price = dis/milePerGallon*fp;
+    price = price.toFixed(2);
+    fuelCost.innerHTML = "Estimate Cost: $ " + price;
 } 
 
 console.log(GetFuelPrice(20));
