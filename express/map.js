496cc49d40537a34b3f6f70808f8dc64b8449bee
Josh Yang <330909261y@gmail.com>
2022-04-14 12:02:24 -0500

    modified map results html

--- express/map.js
+++ express/map.js
@@ -11,3 +11,12 @@ function changeName() {
     alert(input);
 
 }
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
