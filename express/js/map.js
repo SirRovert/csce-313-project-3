0deae2be7ef620da96a2075f8327f7dc253427ca
Josh Yang <330909261y@gmail.com>
2022-04-14 10:54:11 -0500

    modified map_results.html

--- express/js/map.js
+++ express/js/map.js
@@ -2,3 +2,12 @@
 function mapUpdate(){
     console.log("What is this output?")
 }
+
+
+function changeName() {
+    var input = document.getElementById("dest").value;
+    document.getElementById("start").innerHTML = input;
+    console.log(input);
+    alert(input);
+
+}
