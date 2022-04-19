61411909acd378f4716ff98885c39277b3e32f8c
Josh Yang <330909261y@gmail.com>
2022-04-19 08:18:06 -0500

    added changes to map results html

--- express/map.js
+++ express/map.js
@@ -1,6 +1,36 @@
 
 function mapUpdate(){
-    console.log("What is this output?")
+    
+    var input = document.getElementById("locationName").value;
+    Longitude = input;
+
+    //removes map div element
+    const removeElem = document.getElementById("map");
+    removeElem.remove();
+
+    //create new div and p element
+    const mapDiv = document.createElement("div");
+    //const paraElem = document.createElement("p");
+    mapDiv.setAttribute("id", "map");
+
+    //create text node and put textnode into the new paraElem
+    //const textNode = document.createTextNode("Map Div Texts!");
+    //paraElem.appendChild(textNode);
+
+    //add paraElem to map div element
+    //mapDiv.appendChild(paraElem);
+
+    //find paraDiv and tag body
+    //const paraDiv = document.getElementById("para");
+    const bodyDiv = document.getElementsByTagName("body");
+    const mainBody = document.getElementById("body1");
+
+    mainBody.appendChild(mapDiv);
+    //paraDiv.appendChild(mapDiv);
+    //paraDiv.appendChild(mapDiv);
+    
+    initMap(48.8566, 2.3522);
+
 }
 
 
@@ -9,14 +39,19 @@ function changeName() {
     document.getElementById("start").innerHTML = input;
     console.log(input);
     alert(input);
-
 }
+
 
 let map;
 
-function initMap() {
+function initMap(Latitude = 35.676, Longitude = 139.650) {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: Latitude, lng: Longitude },
     zoom: 8,
+    mapTypeID: google.maps.MapTypeId.ROADMAP
   });
 }
+
+
+window.initMap = initMap;
+
