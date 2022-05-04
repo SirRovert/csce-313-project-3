69313c933c4e79c707f9ef7c2144d2548a265165
Josh Yang <330909261y@gmail.com>
2022-05-04 17:25:17 -0500

    Added map

--- express/js/map.js
+++ express/js/map.js
@@ -1,75 +1,127 @@
+// This example requires the Places library. Include the libraries=places
+// parameter when you first load the API. For example:
+// <script
+// src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    mapTypeControl: false,
+    center: { lat: -33.8688, lng: 151.2195 },
+    zoom: 13,
+  });
+  
+  new AutocompleteDirectionsHandler(map);
+}
 
-const locationButton = document.getElementById("getLocationButton");
-locationButton.addEventListener("click", function() {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      let lat = position.coords.latitude;
-      let long = position.coords.longitude;
-      console.log(lat.toFixed(2));
-      console.log(long.toFixed(2));
-      map = new google.maps.Map(document.getElementById("Home"), {
-        center: { lat: parseInt(lat), lng: parseInt(long) },
-        zoom: 8,
-        mapTypeID: google.maps.MapTypeId.ROADMAP
-      });
 
+class AutocompleteDirectionsHandler {
+  map;
+  originPlaceId;
+  destinationPlaceId;
+  travelMode;
+  directionsService;
+  directionsRenderer;
+  places;
+  infoWindow;
+
+  constructor(map) {
+    this.map = map;
+    this.originPlaceId = "";
+    this.destinationPlaceId = "";
+    this.travelMode = google.maps.TravelMode.WALKING;
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+    this.directionsRenderer.setMap(map);
+
+    const originInput = document.getElementById("origin-input");
+    const destinationInput = document.getElementById("destination-input");
+    const modeSelector = document.getElementById("mode-selector");
+    const searchButton = document.getElementById("find");
+
+    // Specify just the place data fields that you need.
+    const originAutocomplete = new google.maps.places.Autocomplete(
+      originInput,
+      { fields: ["place_id"] }
+    );
+    // Specify just the place data fields that you need.
+    const destinationAutocomplete = new google.maps.places.Autocomplete(
+      destinationInput,
+      { fields: ["place_id"] }
+    );
+
+    this.setupClickListener(
+      "changemode-walking",
+      google.maps.TravelMode.WALKING
+    );
+
+    this.setupClickListener(
+      "changemode-transit",
+      google.maps.TravelMode.TRANSIT
+    );
+
+    this.setupClickListener(
+      "changemode-driving",
+      google.maps.TravelMode.DRIVING
+    );
+    this.setupPlaceChangedListener(originAutocomplete, "ORIG");
+    this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
+    //this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+    //this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+    //this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+  }
+  // Sets a listener on a radio button to change the filter type on Places
+  // Autocomplete.
+  setupClickListener(id, mode) {
+    const radioButton = document.getElementById(id);
+
+    radioButton.addEventListener("click", () => {
+      this.travelMode = mode;
+      this.route();
     });
-  });
+  }
+  setupPlaceChangedListener(autocomplete, mode) {
+    autocomplete.bindTo("bounds", this.map);
+    autocomplete.addListener("place_changed", () => {
+      const place = autocomplete.getPlace();
 
-function mapUpdate(){
-    
-    var startVal = document.getElementById("start").value;
-    var destVal = document.getElementById("dest").value;
+      if (!place.place_id) {
+        window.alert("Please select an option from the dropdown list.");
+        return;
+      }
+
+      if (mode === "ORIG") {
+        this.originPlaceId = place.place_id;
+      } else {
+        this.destinationPlaceId = place.place_id;
+      }
+
+      this.route();
+    });
+  }
 
 
-    //removes map div element
-    const removeElem = document.getElementById("map");
-    removeElem.remove();
 
-    //create new div and p element
-    const mapDiv = document.createElement("div");
-    //const paraElem = document.createElement("p");
-    mapDiv.setAttribute("id", "map");
+  route() {
+    if (!this.originPlaceId || !this.destinationPlaceId) {
+      return;
+    }
 
-    //create text node and put textnode into the new paraElem
-    //const textNode = document.createTextNode("Map Div Texts!");
-    //paraElem.appendChild(textNode);
+    const me = this;
 
-    //add paraElem to map div element
-    //mapDiv.appendChild(paraElem);
-
-    //find paraDiv and tag body
-    //const paraDiv = document.getElementById("para");
-    const bodyDiv = document.getElementsByTagName("body");
-    const mainBody = document.getElementById("body1");
-
-    const API3 = document.getElementById("API3");
-    API3.appendChild(mapDiv);
-    //paraDiv.appendChild(mapDiv);
-    //paraDiv.appendChild(mapDiv);
-    
-    initMap(parseInt(startVal), parseInt(destVal));
-
+    this.directionsService.route(
+      {
+        origin: { placeId: this.originPlaceId },
+        destination: { placeId: this.destinationPlaceId },
+        travelMode: this.travelMode,
+      },
+      (response, status) => {
+        if (status === "OK") {
+          me.directionsRenderer.setDirections(response);
+        } else {
+          window.alert("Directions request failed due to " + status);
+        } 
+      }
+    );
+  }
 }
-
-
-function changeName() {
-    var input = document.getElementById("dest").value;
-    document.getElementById("start").innerHTML = input;
-    console.log(input);
-    alert(input);
-}
-
-
-let map;
-
-function initMap(Latitude = 35.676, Longitude = 139.650) {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: Latitude, lng: Longitude },
-    zoom: 8,
-    mapTypeID: google.maps.MapTypeId.ROADMAP
-  });
-}
-
 
 window.initMap = initMap;
-
